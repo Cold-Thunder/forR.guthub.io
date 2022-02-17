9411da5850ff6654a16e1d4f2f2c5cfd63603c3c
Mohammad Hemal <97729902+Cold-Thunder@users.noreply.github.com>
2022-02-17 14:04:39 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-var images = document.querySelector("img");
+
+var images1 = document.querySelector("#image-1");
 
 function myR(){
-    images.src="images/love.png";
+    images1.src="images/love.png";
     document.querySelectorAll("h1")[0].innerHTML="My";
-    document.querySelectorAll("h1")[1].innerHTML="I am so sorry for Everything..<br>Pleaseeee Give Up...";
+    document.querySelectorAll("h1")[1].innerHTML="I know why are you so huff on me. I am so sorry for Everything..<br>Pleaseeee forgive me...";
 }
-var forButton = document.querySelector("button").addEventListener("click", function(){
-    images.classList.add("img-anim");
-    var style = document.querySelector("button");
+var forButton = document.querySelectorAll("button")[0].addEventListener("click", function(){
+    images1.classList.add("img-anim");
+    var style = document.querySelectorAll("button")[0];
     style.classList.add("button");
 })
+
+
+
+
+var images2 = document.querySelector("#image-2");
+
+function myR2(){
+    images2.src="images/love 2.png";
+    document.querySelectorAll("h1")[0].innerHTML="I Love you too.<br>And I will love you forever. If you want,<br> I will wait for you.";
+    
+}
+var forButton = document.querySelectorAll("button")[1].addEventListener("click", function(){
+    images2.classList.add("image-2");
+    var style = document.querySelectorAll("button")[1];
+    style.classList.add("button");
+})
+
