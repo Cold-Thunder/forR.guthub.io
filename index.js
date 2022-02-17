ad25bd32e1e6f3dd55d6e3f4d48562e611dd1a56
Mohammad Hemal <97729902+Cold-Thunder@users.noreply.github.com>
2022-02-17 11:52:41 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var images = document.querySelector("img");
 function myR(){
     images.src="images/love.png";
     document.querySelectorAll("h1")[0].innerHTML="My";
-    document.querySelectorAll("h1")[1].innerHTML="I am so sorry for Everything..<br>Pleaseeee Give Up...";
+    document.querySelectorAll("h1")[1].innerHTML="I am so sorry for Everything..<br>Pleaseeee Forgive Me...";
 }
 var forButton = document.querySelector("button").addEventListener("click", function(){
     images.classList.add("img-anim");
